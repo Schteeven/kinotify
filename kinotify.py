b739495d2621f1530c76065e5a93c74340ee84c0
stevino <xsinko1@fi.muni.cz>
2024-02-15 17:25:16 +0100

    added using newest watchlist, fixed minor refresh bug

--- kinotify.py
+++ kinotify.py
@@ -19,27 +19,39 @@ def get_lb_watchlist(username_text, password_text):
     username = driver.find_element(By.XPATH, "//input[@name='username']")
     password = driver.find_element(By.XPATH, "//input[@name='password']")
 
-    # TODO:
-    username_text = "s_teek"
-    password_text = ""
-
     username.send_keys(username_text)
     password.send_keys(password_text)
 
     driver.find_element(By.XPATH, "//input[@value='Sign in']").click()
     cookies = driver.get_cookies()
+    time.sleep(5)
 
-    watchlist = "https://letterboxd.com/" + username_text + "/watchlist/export"
-    driver.get(watchlist)
+
+    watchlist = "https://letterboxd.com/" + username_text + "/watchlist"
+    
     for cookie in cookies:
         driver.add_cookie(cookie)
-    params = {'behavior': 'allow', "downloadPath" : os.getcwd()}
+    driver.refresh()
+    
+    params = {'behavior': 'allow', "downloadPath" : os.getcwd() + "/watchlist", }
     driver.execute_cdp_cmd("Page.setDownloadBehavior", params)
 
-    driver.refresh()
+    driver.get(watchlist + "/export")
 
-    path = "//a[@href='/" + username_text + "/watchlist/export/']"
-    driver.find_element(By.XPATH, path).click()
+
+def newest(path):
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
 
 
 get_lb_watchlist(username, password)
+
+file = newest(os.getcwd() + "/watchlist")
+print(file)
+
+# throwaway csfd credentials in order to use english language
+username = 'throwaway666'
+password = "7@L9*96HBx%u#I49"
+
+time.sleep(10)
